b8c22c34fc6457b66618d7bb6feb623582f78c53
blockdoth <pepijn.pve@gmail.com>
2025-08-23 00:03:53 +0200

    working on handling http

--- main.hs
+++ main.hs
@@ -2,17 +2,26 @@
 import Control.Concurrent (forkFinally)
 import qualified Control.Exception as E
 import Control.Monad (unless, forever, void)
-import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S
 import qualified Data.List.NonEmpty as NE
 import Network.Socket
-import Network.Socket.ByteString (recv, sendAll)
+import Network.Socket.ByteString (recv, send)
 import Data.ByteString (ByteString)
+import Data.List (isPrefixOf)
 
 maxConn :: Int
 maxConn = 1024
 
+recvBufferSize :: Int
+recvBufferSize = 1024
+
+
+port :: String
+port = "3000"
+
+
 main :: IO ()
-main = runTCPServer Nothing "3000" serve
+main = runTCPServer Nothing port serve
 
 
 runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO a) -> IO a
@@ -38,9 +47,22 @@ runTCPServer mhost port handleConn = do
 
 serve :: Socket -> IO ()
 serve socket = do
-        msg <- recv socket maxConn
+        msg <- recv socket recvBufferSize
         unless (S.null msg) $ do -- if message is null the client has disconnected
-          handleMsg socket msg
+          let msgStr = S.unpack msg
+          putStrLn msgStr             
+          let response = handleMsg msgStr
+          putStrLn response             
+          send socket $ S.pack $ response
           serve socket
         where
-          handleMsg = sendAll 
+          handleMsg msg   
+              | "GET" `isPrefixOf` msg = "GET request"
+              | "POST" `isPrefixOf` msg ="POST request"
+              | "OPTIONS" `isPrefixOf` msg =
+                  "HTTP/1.1 200 OK\r\n\
+                  \Access-Control-Allow-Origin: http://127.0.0.1:8231\r\n\
+                  \Access-Control-Allow-Methods: POST, GET, OPTIONS\r\n\
+                  \Access-Control-Allow-Headers: Content-Type\r\n\r\n"
+              | otherwise                   = "Unknown request"
+
